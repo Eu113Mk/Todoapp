dac0a11de8e26db4443a3faae9abc4a0f60ad75b
Eu113Mk <amelkemala0000@gmail.com>
2021-05-06 14:46:10 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import List from './components/TodoList'
+import Add from './component/Add';
+import List from './component/TodoList';
+import Filter from './component/Filter';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo APP</h1>
+      <div className="header">
+        <Add />
+        <Filter />
+      </div>
+      <List />
     </div>
   );
 }
